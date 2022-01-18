3d161f5227a210c697cce1a43c888a1713a18442
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 19:12:54 +0300

    Add author endpoints

--- backend/internal/author/handler/http/register.go
+++ backend/internal/author/handler/http/register.go
@@ -6,5 +6,14 @@ import (
 )
 
 func RegisterEndpoints(r *gin.RouterGroup, service domain.AuthorService) {
+	h := NewAuthorHandler(service)
 
+	authorEndpoints := r.Group("author")
+	{
+		authorEndpoints.POST("", h.AddAuthor)
+		authorEndpoints.GET("", h.GetAuthors)
+		authorEndpoints.GET("/:id", h.AddAuthor)
+		authorEndpoints.DELETE("/:id", h.AddAuthor)
+		authorEndpoints.PUT("/:id", h.AddAuthor)
+	}
 }
