9ef306d77e3982e385c47c8f7ea3c4f7d9d65776
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 15:39:42 +0300

    Add domain book

--- backend/internal/domain/book.go
+++ backend/internal/domain/book.go
@@ -1,1 +1,23 @@
 package domain
+
+type Book struct {
+	Id    int    `json:"-" db:"id"`
+	Title string `json:"title" binding:"required" db:"title"`
+	Date  string `json:"year" binding:"required" db:"publication_date"'`
+}
+
+type BookService interface {
+	GetBooks()
+	GetBookById()
+	AddBook()
+	UpdateBook()
+	DeleteBook()
+}
+
+type BookRepository interface {
+	GetBooks()
+	GetBookById()
+	AddBook()
+	UpdateBook()
+	DeleteBook()
+}
