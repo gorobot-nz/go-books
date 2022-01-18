e09958f6989be6dce0bc5d21bfeff2b2803bdd81
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 16:17:52 +0300

    Add domain author

--- backend/internal/domain/author.go
+++ backend/internal/domain/author.go
@@ -1,1 +1,23 @@
 package domain
+
+type Author struct {
+	Id      int    `json:"-" db:"id"`
+	Name    string `json:"name" binding:"required" db:"name"`
+	Surname string `json:"surname" binding:"required" db:"surname"`
+}
+
+type AuthorService interface {
+	AddAuthor()
+	GetAuthors()
+	GetAuthorById()
+	DeleteAuthor()
+	UpdateAuthor()
+}
+
+type AuthorRepository interface {
+	AddAuthor()
+	GetAuthors()
+	GetAuthorById()
+	DeleteAuthor()
+	UpdateAuthor()
+}
