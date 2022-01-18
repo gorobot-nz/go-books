e075dbfdd67dbbd961ad552ddaa046e7cb92d44b
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 19:15:16 +0300

    Add book endpoints

--- backend/internal/book/handler/http/register.go
+++ backend/internal/book/handler/http/register.go
@@ -6,5 +6,14 @@ import (
 )
 
 func RegisterEndpoints(r *gin.RouterGroup, service domain.BookService) {
+	h := NewBookHandler(service)
 
+	bookEndpoints := r.Group("/book")
+	{
+		bookEndpoints.POST("", h.AddBook)
+		bookEndpoints.GET("", h.GetBooks)
+		bookEndpoints.GET("/:id", h.GetBookById)
+		bookEndpoints.PUT("/:id", h.UpdateBook)
+		bookEndpoints.DELETE("/:id", h.DeleteBook)
+	}
 }
