6eeb89c1bc865d24b399267aedbf0c164623d8e4
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 18:51:47 +0300

    Add app struct

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,1 +1,42 @@
 package server
+
+import (
+	authorPostgres "github.com/gorobot-nz/go-books/internal/author/repository/postgres"
+	authorService "github.com/gorobot-nz/go-books/internal/author/service"
+	bookPostgres "github.com/gorobot-nz/go-books/internal/book/repository/postgres"
+	bookService "github.com/gorobot-nz/go-books/internal/book/service"
+	userPostgres "github.com/gorobot-nz/go-books/internal/user/repository/postgres"
+	userService "github.com/gorobot-nz/go-books/internal/user/service"
+
+	"github.com/gorobot-nz/go-books/internal/domain"
+
+	"github.com/jmoiron/sqlx"
+
+	"net/http"
+)
+
+type App struct {
+	server *http.Server
+
+	userService   domain.UserService
+	bookService   domain.BookService
+	authorService domain.AuthorService
+}
+
+func NewApp() *App {
+	dbConnection := initDb()
+
+	userRepository := userPostgres.NewUserRepository(dbConnection)
+	bookRepository := bookPostgres.NewBookRepository(dbConnection)
+	authorRepository := authorPostgres.NewAuthorRepository(dbConnection)
+
+	return &App{
+		userService:   userService.NewUserService(userRepository),
+		bookService:   bookService.NewBookService(bookRepository),
+		authorService: authorService.NewAuthorService(authorRepository),
+	}
+}
+
+func initDb() *sqlx.DB {
+	return nil
+}
