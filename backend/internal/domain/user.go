738b4223930b0f3608d8206f23e5ac7dd303f096
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-18 15:31:52 +0300

    Add user domain

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -1,1 +1,17 @@
 package domain
+
+type User struct {
+	Id       int    `json:"-" db:"id"`
+	Username string `json:"username" binding:"required" db:"username"'`
+	Password string `json:"password" binding:"required" db:"password"`
+}
+
+type UserService interface {
+	SignUp()
+	SignIn()
+}
+
+type UserRepository interface {
+	SignUp()
+	SignIn()
+}
