85cbcf2296ba78aa74d718c565f2239783fb93d3
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-19 20:12:58 +0300

    Add books service mock

---
+++ backend/internal/book/handler/http/handler_test.go
@@ -0,0 +1,18 @@
+package http
+
+import "testing"
+
+func TestBookHandler_AddBook(t *testing.T) {
+}
+
+func TestBookHandler_GetBooks(t *testing.T) {
+}
+
+func TestBookHandler_GetBookById(t *testing.T) {
+}
+
+func TestBookHandler_DeleteBook(t *testing.T) {
+}
+
+func TestBookHandler_UpdateBook(t *testing.T) {
+}
