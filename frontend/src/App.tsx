83a559deb7448259d874ca81a1dc13ed6e812123
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-01-22 22:21:44 +0300

    Fix signup and signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppRouter from "./components/AppRouter";
 import {Layout} from "antd";
 import "./App.css"
+import {useDispatch} from "react-redux";
+import {AuthActionCreators} from "./store/reducers/auth/action-creators";
 
 function App() {
+    const dispatcher = useDispatch()
+    useEffect(() => {
+        if (localStorage.getItem('token')){
+            dispatcher(AuthActionCreators.setIsAuth(true))
+        }
+    }, [])
+
     return (
         <Layout>
             <Layout.Content>
